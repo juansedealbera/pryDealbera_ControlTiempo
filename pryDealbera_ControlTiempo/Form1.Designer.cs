aa3a9d188a6a2949b7cf330e2388816ea2bf8a65
Alumno <Alumno@PC26>
2025-05-12 13:03:27 -0300

    Agregar archivos de proyecto.

---
+++ pryDealbera_ControlTiempo/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace pryDealbera_ControlTiempo
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTrabajo = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTrabajo
+            // 
+            this.btnTrabajo.Location = new System.Drawing.Point(140, 92);
+            this.btnTrabajo.Name = "btnTrabajo";
+            this.btnTrabajo.Size = new System.Drawing.Size(79, 28);
+            this.btnTrabajo.TabIndex = 0;
+            this.btnTrabajo.Text = "Trabajo";
+            this.btnTrabajo.UseVisualStyleBackColor = true;
+            this.btnTrabajo.Click += new System.EventHandler(this.btnTrabajo_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 235);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(363, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(50, 17);
+            this.toolStripStatusLabel1.Text = "Tiempo:";
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 257);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnTrabajo);
+            this.Name = "frmInicio";
+            this.Text = "Inicio Sesion";
+            this.Load += new System.EventHandler(this.frmInicio_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTrabajo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ pryDealbera_ControlTiempo/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryDealbera_ControlTiempo
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private DateTime inicioTrabajo;
+        private Timer timer;
+        private bool enTrabajo = false;
+
+        private void btnTrabajo_Click(object sender, EventArgs e)
+        {
+            if (!enTrabajo)
+            { 
+                //inicio
+                enTrabajo = true;
+                inicioTrabajo = DateTime.Now;
+                btnTrabajo.Text = "Finalizar";
+                timer.Start();
+            }
+            else
+            {
+                //fin
+                enTrabajo = false;
+                timer.Stop();
+                TimeSpan tiempoTrabajado = DateTime.Now - inicioTrabajo;
+                MessageBox.Show($"Tiempo trabajado: {tiempoTrabajado:hh\\:mm\\:ss}");
+                btnTrabajo.Text = "Trabajo";
+                toolStripStatusLabel1.Text = "Tiempo: 00:00:00";
+            }
+        }
+
+        private void frmInicio_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 1000; // 1 segundo
+            timer.Tick += Timer_Tick;
+
+            toolStripStatusLabel1.Text = "Tiempo: 00:00:00";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan tiempoActual = DateTime.Now - inicioTrabajo;
+            toolStripStatusLabel1.Text = "Tiempo: " + tiempoActual.ToString(@"hh\:mm\:ss");
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
